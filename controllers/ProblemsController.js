09c50303c30d5a9887a5bc45edd736032b7c4bbb
Tony Lopez <tlopez@xpanxion.com>
2016-07-07 14:58:59 -0500

    Adding angular connection to github

--- controllers/ProblemsController.js
+++ controllers/ProblemsController.js
@@ -4,12 +4,26 @@
         .controller('problemsCtrl', ProblemsController, ['$scope', 'problemService']);
 
     function ProblemsController($scope, problemService) {
-        $scope.activeProblems = [];
-        $scope.init = init;
+        $scope.allProblems = [];
+        $scope.problemCategories = [];
+        $scope.problemsByCategory = [];
 
-        function init(category) {
-            $scope.activeProblems = problemService.getByCategory(category);
+        function init() {
+            
+            problemService.getByCategoriesAsMap().then(function(map) {
+                $scope.problemsByCategory = map;
+            });
+
+            problemService.getAll().then(function(problems) {
+                $scope.allProblems = problems;
+            });
+
+            problemService.getCategories().then(function(response) {
+                $scope.problemCategories = response.data;
+            });
         }
+        
+        init();
     }
 
 }())
--- services/ProblemService.js
+++ services/ProblemService.js
@@ -1,47 +1,89 @@
 (function() {
 
     angular.module('pp-app')
-        .factory('problemService', ProblemService, []);
+        .factory('problemService', ProblemService, ['$http', '$q']);
 
-    function ProblemService() {
+    function ProblemService($http, $q) {
+
+        const GITHUB_REPO_OWNER = 'lopezton';
+        const GITHUB_REPO_NAME = 'practice-problems'
+        const GITHUB_API_PROBLEMS = 'https://api.github.com/repos/' + GITHUB_REPO_OWNER + '/' + GITHUB_REPO_NAME + '/contents/problems';
+        const GITHUB_RAW_DOMAIN = 'https://raw.githubusercontent.com/';
+        const GITHUB_RAW_REPOSITORY = GITHUB_RAW_DOMAIN + GITHUB_REPO_OWNER + '/' + GITHUB_REPO_NAME;
+        const GITHUB_PROBLEM_CATEGORY_RAW_URL = GITHUB_RAW_REPOSITORY + '/gh-pages/problem-category.json';
+
         return {
             getAll: getAll,
-            getByCategory: getByCategory
+            getByCategoriesAsMap: getByCategoriesAsMap,
+            getCategories: getCategories
         };
 
         function getAll() {
-            var rv = [];
-            for(var i = 1; i <= 6; i++) {
-                rv.push(createMockProject(i));
-            }
-            return rv;
+
+            var deferred = $q.defer();
+
+            $http.get(GITHUB_API_PROBLEMS).then(function(response) {
+
+                var problems = [];
+                var promises = [];
+
+                angular.forEach(response.data, function(problemDirectoryInfo) {
+                    promises.push(getById(problemDirectoryInfo.name).then(function(problem) {
+                        problems.push(problem);
+                    }));
+                });
+
+                $q.all(promises).then(function() {
+                    deferred.resolve(problems);
+                }, function(response) {
+                    deferred.reject(response);
+                });
+            });
+
+            return deferred.promise;
         }
 
-        function getByCategory(category) {
-            var rv = [];
-            if ('All' === category) {
-                rv = getAll();
-            } else if ('Easy' === category) {
-                for(var i = 1; i <= 3; i++) {
-                    rv.push(createMockProject(i));
-                }
-            } else if ('Medium' === category) {
-                for(var i = 4; i <= 5; i++) {
-                    rv.push(createMockProject(i));
-                }
-            } else if ('Hard' === category) {
-                rv.push(createMockProject(6));
-            }
-            return rv;
+        function getCategories() {
+            return $http.get(GITHUB_PROBLEM_CATEGORY_RAW_URL);
         }
 
-        function createMockProject(index) {
-            return {
-                name: 'Project Name ' + index,
-                thumbnailUrl: 'http://placehold.it/700x400',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, ' +
-                    'gravida pellentesque urna varius vitae.'
-            };
+        function getByCategoriesAsMap() {
+            var deferred = $q.defer();
+
+            getAll().then(function(problems) {
+                var problemsMap = [];
+                angular.forEach(problems, function(problem) {
+                    problemsMap[problem.id] = problem;
+                });
+
+                getCategories().then(function(response) {
+                    var categories = response.data;
+                    var map = {};
+
+                    angular.forEach(categories, function(category) {
+                        map[category.name] = [];
+                        angular.forEach(category.problems, function(problemName) {
+                            map[category.name].push(problemsMap[problemName]);
+                        })
+                    });
+                    
+                    deferred.resolve(map);
+                });
+            });
+            
+            return deferred.promise;
+        }
+
+        function getById(problemId) {
+            var deferred = $q.defer();
+
+            $http.get(GITHUB_RAW_REPOSITORY + '/gh-pages/problems/' + problemId + '/problem.json').then(function(response) {
+                    deferred.resolve(response.data);
+            }, function(response) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
         }
     }
 
